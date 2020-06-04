e45d9dec42576ee85dbf9a6647e5c7d359c694d2
Stuart Lewis <stuart@stuartlewis.com>
2020-06-04 20:41:42 +0100

    Removing wrongly committed files

--- app/Views/support.php
+++
@@ -1,7 +0,0 @@
-<main role="main" class="container">
-    <h1 class="mt-5">Support</h1>
-    <p class="lead">Help will be here!</p>
-    <p>
-        Stuff about help, how to search, etc.
-    </p>
-</main>
