41f95ef5b4225e535cc7628c45fa993c632f9cc0
Stuart Lewis <stuart@stuartlewis.com>
2021-01-07 13:56:44 +0000

    Include new urlTEI field

--- src/main/java/world/opentexts/validate/Validator.java
+++ src/main/java/world/opentexts/validate/Validator.java
@@ -74,17 +74,19 @@ public class Validator {
                                 break;
                         case 14: expected = "urlALTOXML";
                                 break;
-                        case 15: expected = "urlOther";
+                        case 15: expected = "urlTEI";
                                 break;
-                        case 16: expected = "placeOfPublication";
+                        case 16: expected = "urlOther";
                                 break;
-                        case 17: expected = "licence";
+                        case 17: expected = "placeOfPublication";
                                 break;
-                        case 18: expected = "idOther";
+                        case 18: expected = "licence";
                                 break;
-                        case 19: expected = "catLink";
+                        case 19: expected = "idOther";
                                 break;
-                        case 20: expected = "language";
+                        case 20: expected = "catLink";
+                                break;
+                        case 21: expected = "language";
                                 break;
                         default: expected = "$.$";
                                 System.err.println("  - ERROR: Incorrect headings - too many fields");
@@ -100,6 +102,7 @@ public class Validator {
             } else {
                 // Now check each field meets the requirements
                 int position = 0;
+                //System.out.println(record);
                 for (String value : record) {
                     // Set the organisation if required
                     if (org == null) {
@@ -114,26 +117,27 @@ public class Validator {
                                 System.err.println("  - Organsiation ('" + value + "') should be '" + org + "'");
                                 System.exit(0);
                             }
-                            this.checkNonRepeatable(lineCounter, "organisation", value);
+                            this.checkNonRepeatable(lineCounter, "organisation", value, record);
                             break;
                         case 2: // idLocal
-                            this.checkMandatory(lineCounter, "idLocal", value);
-                            this.checkNonRepeatable(lineCounter, "idLocal", value);
+                            this.checkMandatory(lineCounter, "idLocal", value, record);
+                            this.checkNonRepeatable(lineCounter, "idLocal", value, record);
                             break;
                         case 3: // title
-                            this.checkMandatory(lineCounter, "title", value);
-                            this.checkNonRepeatable(lineCounter, "title", value);
+                            this.checkMandatory(lineCounter, "title", value, record);
+                            this.checkNonRepeatable(lineCounter, "title", value, record);
                             break;
                         case 4: // urlMain
-                            this.checkMandatory(lineCounter, "urlMain", value);
-                            this.checkNonRepeatable(lineCounter, "urlMain", value);
+                            this.checkMandatory(lineCounter, "urlMain", value, record);
+                            this.checkNonRepeatable(lineCounter, "urlMain", value, record);
+                            this.checkIsHttp(lineCounter, "urlMain", value, record);
                             break;
                         case 5: // year
-                            this.checkNonRepeatable(lineCounter, "year", value);
-                            this.checkYear(lineCounter, "year", value);
+                            this.checkNonRepeatable(lineCounter, "year", value, record);
+                            this.checkYear(lineCounter, "year", value, record);
                             break;
                         case 6: // date
-                            this.checkNonRepeatable(lineCounter, "date", value);
+                            this.checkNonRepeatable(lineCounter, "date", value, record);
                             break;
                         case 7: // publisher
                             break;
@@ -144,32 +148,41 @@ public class Validator {
                         case 10: // description
                             break;
                         case 11: // urlPDF
-                            this.checkNonRepeatable(lineCounter, "urlPDF", value);
+                            this.checkNonRepeatable(lineCounter, "urlPDF", value, record);
+                            this.checkIsHttp(lineCounter, "urlPDF", value, record);
                             break;
                         case 12: // urlIIIF
-                            this.checkNonRepeatable(lineCounter, "urlIIIF", value);
+                            this.checkNonRepeatable(lineCounter, "urlIIIF", value, record);
+                            this.checkIsHttp(lineCounter, "urlIIIF", value, record);
                             break;
                         case 13: // urlPlainText
-                            this.checkNonRepeatable(lineCounter, "urlPlainText", value);
+                            this.checkNonRepeatable(lineCounter, "urlPlainText", value, record);
+                            this.checkIsHttp(lineCounter, "urlPlainText", value, record);
                             break;
                         case 14: // urlALTOXML
-                            this.checkNonRepeatable(lineCounter, "urlALTOXML", value);
+                            this.checkNonRepeatable(lineCounter, "urlALTOXML", value, record);
+                            this.checkIsHttp(lineCounter, "urlALTOXML", value, record);
                             break;
-                        case 15: // urlOther
+                        case 15: // urlTEI
+                            this.checkNonRepeatable(lineCounter, "urlTEI", value, record);
+                            this.checkIsHttp(lineCounter, "urlTEI", value, record);
                             break;
-                        case 16: // placeOfPublication
+                        case 16: // urlOther
                             break;
-                        case 17: // licence
-                            this.checkNonRepeatable(lineCounter, "licence", value);
+                        case 17: // placeOfPublication
                             break;
-                        case 18: // idOther
+                        case 18: // licence
+                            this.checkNonRepeatable(lineCounter, "licence", value, record);
                             break;
-                        case 19: // catLink
-                            this.checkNonRepeatable(lineCounter, "catLink", value);
-                             break;
-                        case 20: // language
-                            this.checkNonRepeatable(lineCounter, "language", value);
-                             break;
+                        case 19: // idOther
+                            break;
+                        case 20: // catLink
+                            this.checkNonRepeatable(lineCounter, "catLink", value, record);
+                            this.checkIsHttp(lineCounter, "catLink", value, record);
+                            break;
+                        case 21: // language
+                            this.checkNonRepeatable(lineCounter, "language", value, record);
+                            break;
                         default: // Something has gone wrong
                             System.err.println(" - ERROR on line " + lineCounter);
                             System.err.println("  - Unexpected value (" + value + ") - too many fields");
@@ -185,56 +198,71 @@ public class Validator {
         System.out.println("   - Processed " + lineCounter + " records\n");
     }
     
-    private void checkMandatory(int lineCounter, String field, String value) {
+    private void checkMandatory(int lineCounter, String field, String value, CSVRecord record) {
         if (value.equals("")) {
             System.err.println(" - ERROR on line " + lineCounter);
             System.err.println("  - " + field + " cannot be empty, it is a MANDATORY field");
+            System.err.println(record);
             System.exit(0);
         }
     }
     
-    private void checkNonRepeatable(int lineCounter, String field, String value) {
+    private void checkNonRepeatable(int lineCounter, String field, String value, CSVRecord record) {
         if (value.contains("|")) {
             System.err.println(" - ERROR on line " + lineCounter);
             System.err.println("  - " + field + " is not a repeatable field:");
             System.err.println("  - " + value);
+            System.err.println(record);
             System.exit(0);
         }
     }
     
-    private void checkYear(int lineCounter, String field, String value) {
+    private void checkIsHttp(int lineCounter, String field, String value, CSVRecord record) {
+        if ((!value.startsWith("http")) && (!"".equals(value))) {
+            System.err.println(" - ERROR on line " + lineCounter);
+            System.err.println("  - " + field + " does not look like a http/s link:");
+            System.err.println("  - " + value);
+            System.err.println(record);
+            System.exit(0);
+        }
+    }
+    
+    private void checkYear(int lineCounter, String field, String value, CSVRecord record) {
         if (value.equals("")) return;
         try {
             int y = Integer.parseInt(value);
-            if (y < 1000) {
+            if (y < -550) {
                 System.err.println(" - ERROR on line " + lineCounter);
-                System.err.println("  - " + field + " is < 1000:");
+                System.err.println("  - " + field + " is < 250:");
                 System.err.println("  - " + value);
+                System.err.println(record);
                 System.exit(0);
             }
             if (y > 2025) {
                 System.err.println(" - ERROR on line " + lineCounter);
                 System.err.println("  - " + field + " is > 2025:");
                 System.err.println("  - " + value);
+                System.err.println(record);
                 System.exit(0);
             }    
         } catch (NumberFormatException e) {
             System.err.println(" - ERROR on line " + lineCounter);
             System.err.println("  - " + field + " is not a number:");
             System.err.println("  - '" + value + "'");
+            System.err.println(record);
             System.exit(0);
         }
     }
     
     public static void main(String[] args) {
         // Take the filename as the only parameter
-        if (args.length < 1) {
+        /**if (args.length < 1) {
             System.err.println("Please supply a filename to validate");
             System.exit(0);
-        }
+        }*/
         
         try {
-            Validator v = new Validator(args[0]);   
+            Validator v = new Validator("c:\\otw\\cul.csv");   
         } catch (Exception e) {
             System.err.println("Error encountered:");
             System.err.println(" - " + e.getMessage() + "\n");
